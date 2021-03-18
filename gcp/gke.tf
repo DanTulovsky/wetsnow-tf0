a672ceef1db47ec6d871a8f590b7ac167c2f338f
Dan Tulovsky <dant@wetsnow.com>
2021-03-17 22:24:13 -0400

    fewer nodes

--- gcp/gke.tf
+++ gcp/gke.tf
@@ -45,7 +45,7 @@ module "gke" {
       machine_type = "e2-small"
       #   min_count          = 3
       #   max_count          = 3
-      node_count         = 2
+      node_count         = 1
       local_ssd_count    = 0
       disk_size_gb       = 100
       disk_type          = "pd-standard"
