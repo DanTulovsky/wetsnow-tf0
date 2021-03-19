dbc8a582439d06b861a8a10c993a930e7ebcfb8e
Dan Tulovsky <dant@wetsnow.com>
2021-03-19 13:18:45 -0400

    new cert

--- modules/http-ingress/managed-certs.tf
+++ modules/http-ingress/managed-certs.tf
@@ -1,23 +1,35 @@
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_managed_ssl_certificate
-resource "google_compute_managed_ssl_certificate" "wetsnow-com-20210319" {
-  name = "wetsnow-cert"
 
+locals {
+  domains = [
+    "wetsnow.com.",
+    "www.wetsnow.com.",
+    "ambassador-admin.wetsnow.com.",
+    "pepper-poker.wetsnow.com.",
+    "grafana.wetsnow.com.",
+    "login.wetsnow.com.",
+    "prometheus.wetsnow.com.",
+    "pgadmin.wetsnow.com.",
+    "kafka-ui.wetsnow.com.",
+    "dusselskolk.com.",
+    "www.dusselskolk.com.",
+  ]
+}
+
+resource "google_compute_managed_ssl_certificate" "wetsnow-com" {
+  name = "wetsnow-cert"
   managed {
-    domains = [
-      "wetsnow.com.",
-      "www.wetsnow.com.",
-      "ambassador-admin.wetsnow.com.",
-      "pepper-poker.wetsnow.com.",
-      "grafana.wetsnow.com.",
-      "login.wetsnow.com.",
-      "prometheus.wetsnow.com.",
-      "pgadmin.wetsnow.com.",
-      "kafka-ui.wetsnow.com.",
-      "dusselskolk.com.",
+    domains = local.domains
+  }
+}
+
+resource "google_compute_managed_ssl_certificate" "wetsnow-com-next" {
+  name = "wetsnow-cert-20210319"
+  managed {
+    domains = concat(local.domains, [
       "scope.wetsnow.com",
       "weave-scope.wetsnow.com",
-      "www.dusselskolk.com.",
-    ]
+    ])
   }
 }
