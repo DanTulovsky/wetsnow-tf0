b41a3446093fb21ed4e94ab10ed1db64cfde6faf
Dan Tulovsky <dant@wetsnow.com>
2021-03-19 13:29:07 -0400

    undo cert

--- modules/http-ingress/main.tf
+++ modules/http-ingress/main.tf
@@ -12,7 +12,7 @@ resource "kubernetes_ingress" "ambassador" {
       "kubernetes.io/ingress.global-static-ip-name" : google_compute_global_address.default.name
       # https://github.com/hashicorp/terraform-provider-kubernetes/issues/446
       #   "networking.gke.io/managed-certificates" : google_compute_managed_ssl_certificate.wetsnow-com.name
-      "ingress.gcp.kubernetes.io/pre-shared-cert" : google_compute_managed_ssl_certificate.wetsnow-com-next.name
+      "ingress.gcp.kubernetes.io/pre-shared-cert" : google_compute_managed_ssl_certificate.wetsnow-com.name
       "kubernetes.io/ingress.class" : "gce"
     }
   }
