99d19c4902ede8f5373bd0020a06d4e8e45506c9
Dan Tulovsky <dant@wetsnow.com>
2021-03-23 13:48:42 -0400

    fix url

--- modules/web-static/20-deployment.tf
+++ modules/web-static/20-deployment.tf
@@ -66,7 +66,7 @@ resource "kubernetes_deployment" "frontend" {
             "--version=${var.app_version}",
             "--enable_kafka=false",
             "--kafka_broker=kafka0.kafka",
-            "--quote_server=quote-server-http:8080"
+            "--quote_server=http://quote-server-http:8080"
           ]
 
           port {
