397c5310afb4fcbd792f90dc6656f37a3c4cbc20
Dan Tulovsky <dant@wetsnow.com>
2021-03-31 16:36:43 -0400

    quote server allow only in namespace web

---
+++ modules/quote-server/network-policy.tf
@@ -0,0 +1,40 @@
+resource "kubernetes_network_policy" "quote_server" {
+  metadata {
+    name      = "quote-server-network-policy"
+    namespace = "web"
+  }
+
+  spec {
+    pod_selector {
+      match_expressions {
+        key      = "app"
+        operator = "In"
+        values   = ["quote"]
+      }
+      match_expressions {
+        key      = "component"
+        operator = "In"
+        values   = ["server-http"]
+      }
+    }
+
+    ingress {
+      ports {
+        port     = "http"
+        protocol = "TCP"
+      }
+
+      from {
+        namespace_selector {
+          match_labels = {
+            name = "web"
+          }
+        }
+      }
+    }
+
+    egress {} # single empty rule to allow all egress traffic
+
+    policy_types = ["Ingress", "Egress"]
+  }
+}
