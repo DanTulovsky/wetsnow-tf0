a0ed05e6e2918c9d0db68763542f7f91d828a59d
Dan Tulovsky <dant@lightstep.com>
2021-05-02 12:41:34 -0400

    add service.

--- cluster0/traefik-mesh.tf
+++ cluster0/traefik-mesh.tf
@@ -9,3 +9,36 @@ resource "helm_release" "traefik-mesh" {
   values = [templatefile("${path.module}/yaml/traefik-mesh-values.yaml", {
   })]
 }
+
+
+resource "kubernetes_service" "traefik-maesh-service" {
+  depends_on = [helm_release.traefik-mesh]
+
+  metadata {
+    name      = "traefik-mesh-api"
+    namespace    = module.common.namespaces.traefik
+
+    labels = {
+      app       = "maesh"
+      component = "maesh-mesh"
+      service   = "traefik-maesh"
+    }
+  }
+
+  spec {
+    port {
+      name        = "http"
+      protocol    = "TCP"
+      port        = 8080
+    }
+
+    selector = {
+      app       = "maesh"
+      component = "maesh-mesh"
+    }
+
+    cluster_ip       = "None"
+    type             = "ClusterIP"
+    session_affinity = "None"
+  }
+}
