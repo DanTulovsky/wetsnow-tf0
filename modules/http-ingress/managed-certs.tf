b9bf4e02e4528c52568afe0e261d054513bf0771
Dan Tulovsky <dant@lightstep.com>
2021-05-02 13:03:09 -0400

    certs

--- modules/http-ingress/managed-certs.tf
+++ modules/http-ingress/managed-certs.tf
@@ -24,8 +24,18 @@ resource "google_compute_managed_ssl_certificate" "wetsnow-com" {
   }
 }
 
+resource "google_compute_managed_ssl_certificate" "wetsnow-com-next" {
+  name = "wetsnow-cert-20210319"
+  managed {
+    domains = concat(local.domains, [
+      "scope.wetsnow.com",
+      "weave-scope.wetsnow.com",
+    ])
+  }
+}
 # to add a cert, update the date in the name, add a new cert and
 # update kubernetes_ingress.ambassador in main.tf
+# then remove the previous instance
 # this causes downtime!!
 resource "google_compute_managed_ssl_certificate" "wetsnow-com-20200502" {
   name = "wetsnow-cert-20210502"
