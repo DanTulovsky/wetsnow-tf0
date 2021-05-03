84f2bfedb6a7fa8538c4b6a23788eeaaf3d9a612
Dan Tulovsky <dant@lightstep.com>
2021-05-02 22:35:42 -0400

    statsd

--- modules/open-telemetry/variables.tf
+++ modules/open-telemetry/variables.tf
@@ -29,7 +29,7 @@ variable "otel" {
   type = map(string)
   default = {
 //    metrics_receivers  = "[otlp, k8s_cluster, kubeletstats]"
-    metrics_receivers  = "[otlp]"
+    metrics_receivers  = "[otlp, statsd]"
     metrics_processors = "[memory_limiter, batch]"
     metrics_exporters  = "[otlp/lightstep]"
     trace_receivers    = "[otlp, zipkin, jaeger]"
