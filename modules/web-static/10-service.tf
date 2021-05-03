8957df3e2d1a22986ab8242a22813c182181b4af
Dan Tulovsky <dant@lightstep.com>
2021-05-03 18:37:18 -0400

    quotes

--- modules/web-static/10-service.tf
+++ modules/web-static/10-service.tf
@@ -11,10 +11,10 @@ resource "kubernetes_service" "static_web_frontend" {
       tier      = "production"
     }
     annotations = {
-      mesh.traefik.io/traffic-type: "http"
-      mesh.traefik.io/retry-attempts: "2"
-      mesh.traefik.io/ratelimit-average: "20"
-      mesh.traefik.io/ratelimit-burst: "40"
+      "mesh.traefik.io/traffic-type": "http"
+      "mesh.traefik.io/retry-attempts": "2"
+      "mesh.traefik.io/ratelimit-average": "20"
+      "mesh.traefik.io/ratelimit-burst": "40"
     }
   }
 
