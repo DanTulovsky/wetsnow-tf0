efb7dd2c309335525cf803741f73910958a3f5e5
Dan Tulovsky <dant@lightstep.com>
2021-05-03 18:35:18 -0400

    Add annotations

--- modules/web-static/10-service.tf
+++ modules/web-static/10-service.tf
@@ -10,6 +10,12 @@ resource "kubernetes_service" "static_web_frontend" {
       service   = "static-web-frontend"
       tier      = "production"
     }
+    annotations = {
+      mesh.traefik.io/traffic-type: "http"
+      mesh.traefik.io/retry-attempts: "2"
+      mesh.traefik.io/ratelimit-average: "20"
+      mesh.traefik.io/ratelimit-burst: "40"
+    }
   }
 
   spec {
