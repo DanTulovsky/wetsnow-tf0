02a545a8f4ecbfd1baf80d9fda45d3de40b29610
Dan Tulovsky <dant@lightstep.com>
2021-05-25 22:07:13 -0400

    argo helm chart

---
+++ modules/argo/main.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "argo-rollouts" {
+  name         = "argo-rollouts"
+  namespace    = var.namespace
+  repository   = "https://argoproj.github.io/argo-helm"
+  chart        = "argo-rollouts"
+  wait         = true
+  force_update = false
+
+  values = [templatefile("${path.module}/yaml/values.yaml", {
+    tag: "v1.0.0"
+  })]
+}
---
+++ modules/argo/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+}
