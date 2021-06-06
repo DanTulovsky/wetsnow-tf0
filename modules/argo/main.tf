aa1bce00e48466a824d909014b43f301058d97c5
Dan Tulovsky <dant@lightstep.com>
2021-06-06 09:29:28 -0400

    argo helm

--- modules/argo/main.tf
+++ modules/argo/main.tf
@@ -1,19 +1,13 @@
-//resource "helm_release" "argo-rollouts" {
-//  name         = "argo-rollouts"
-//  namespace    = var.namespace
-//  repository   = "https://argoproj.github.io/argo-helm"
-//  chart        = "argo-rollouts"
-//  wait         = true
-//  force_update = false
-//
-//  values = [templatefile("${path.module}/yaml/values.yaml", {
-//    tag: "v1.0.0"
-//  })]
-//}
+resource "helm_release" "argo-rollouts" {
+  name         = "argo-rollouts"
+  namespace    = var.namespace
+  repository   = "https://argoproj.github.io/argo-helm"
+  chart        = "argo-rollouts"
+  wait         = true
+  force_update = false
+  version = "1.0.1"
 
-// Until the helm chart supports v1.0
-resource "kubectl_manifest" "argo-rollouts" {
-  yaml_body  = templatefile("${path.module}/yaml/k8s/argo.yaml", {
-    namespace = var.namespace
-  })
+  values = [templatefile("${path.module}/yaml/values.yaml", {
+  })]
 }
+
