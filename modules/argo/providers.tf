66bd8b07de9e746f87c3f3292d2eab3de6bee551
Dan Tulovsky <dant@lightstep.com>
2021-06-06 09:42:01 -0400

    argo monitor

--- modules/argo/providers.tf
+++ modules/argo/providers.tf
@@ -4,9 +4,5 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.0"
     }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
   }
 }
