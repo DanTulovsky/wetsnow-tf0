d1816240785d5c5cb992415ecaf53296f12f164c
Dan Tulovsky <dant@lightstep.com>
2021-06-10 09:25:39 -0400

    fix cert name

--- modules/http-ingress/managed-certs.tf
+++ modules/http-ingress/managed-certs.tf
@@ -34,7 +34,7 @@ resource "google_compute_managed_ssl_certificate" "wetsnow-com-20200502" {
 # then remove the previous instance
 # this causes downtime!!
 resource "google_compute_managed_ssl_certificate" "wetsnow-com-20210610" {
-  name = "wetsnow-cert-20210502"
+  name = "wetsnow-cert-20210610"
   managed {
     domains = concat(local.domains, [
       "scope.wetsnow.com",
