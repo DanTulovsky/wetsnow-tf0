a5bfa3b62d7af4f8b8251c7349f3e8242f7d0381
Dan Tulovsky <dant@lightstep.com>
2021-09-10 21:52:47 -0400

    handle errors

--- modules/quote-server/20-deployment.tf
+++ modules/quote-server/20-deployment.tf
@@ -4,16 +4,16 @@ locals {
 
 resource "kubernetes_deployment" "quote_server_http" {
   metadata {
-    name = "quote-server-http"
+    name      = "quote-server-http"
     namespace = var.namespace
   }
 
   spec {
     # managed by Argo Rollouts (yaml/k8s/rollout.yaml)
-    replicas = 0
+    replicas                  = 0
     progress_deadline_seconds = 300
-    revision_history_limit = 5
-    min_ready_seconds = 20
+    revision_history_limit    = 5
+    min_ready_seconds         = 20
     strategy {
       type = "RollingUpdate"
 
@@ -21,19 +21,19 @@ resource "kubernetes_deployment" "quote_server_http" {
 
     selector {
       match_labels = {
-        app = "quote"
+        app       = "quote"
         component = "server-http"
-        tier = "production"
+        tier      = "production"
       }
     }
 
     template {
       metadata {
         labels = {
-          "app" = "quote"
-          "component" = "server-http"
-          "tier" = "production"
-          "service.name" = "quote"
+          "app"             = "quote"
+          "component"       = "server-http"
+          "tier"            = "production"
+          "service.name"    = "quote"
           "service.version" = var.app_version
         }
       }
@@ -41,19 +41,19 @@ resource "kubernetes_deployment" "quote_server_http" {
       spec {
         share_process_namespace = true
         toleration {
-          key = "node-role.kubernetes.io/master"
-          effect = "NoSchedule"
+          key      = "node-role.kubernetes.io/master"
+          effect   = "NoSchedule"
           operator = "Exists"
         }
         topology_spread_constraint {
-          max_skew = 2
-          topology_key = "topology.kubernetes.io/zone"
+          max_skew           = 2
+          topology_key       = "topology.kubernetes.io/zone"
           when_unsatisfiable = "DoNotSchedule"
           label_selector {
             match_labels = {
-              app = "quote"
+              app       = "quote"
               component = "server-http"
-              tier = "production"
+              tier      = "production"
             }
           }
         }
@@ -72,7 +72,7 @@ resource "kubernetes_deployment" "quote_server_http" {
         priority_class_name = var.priority_class
 
         container {
-          name = "server-http"
+          name  = "server-http"
           image = "ghcr.io/dantulovsky/quote-server/server:${var.app_version}"
           args = [
             "--enable_metrics",
@@ -80,17 +80,17 @@ resource "kubernetes_deployment" "quote_server_http" {
           ]
 
           port {
-            name = "http"
+            name           = "http"
             container_port = var.port_http
           }
 
           port {
-            name = "grpc"
+            name           = "grpc"
             container_port = var.port_grpc
           }
 
           env {
-            name = "LS_ACCESS_TOKEN"
+            name  = "LS_ACCESS_TOKEN"
             value = var.lightstep_access_token
           }
 
@@ -100,23 +100,23 @@ resource "kubernetes_deployment" "quote_server_http" {
           # }
 
           env {
-            name = "PORT"
+            name  = "PORT"
             value = var.port_http
           }
 
           env {
-            name = "GIN_MODE"
+            name  = "GIN_MODE"
             value = local.release
           }
 
           resources {
             limits = {
-              cpu = "50m"
+              cpu    = "50m"
               memory = "200Mi"
             }
 
             requests = {
-              cpu = "10m"
+              cpu    = "10m"
               memory = "40Mi"
             }
           }
@@ -127,13 +127,13 @@ resource "kubernetes_deployment" "quote_server_http" {
               port = var.port_http
 
               http_header {
-                name = "X-Healthz-Prober"
+                name  = "X-Healthz-Prober"
                 value = "liveliness"
               }
             }
 
             initial_delay_seconds = 5
-            period_seconds = 5
+            period_seconds        = 5
           }
 
           readiness_probe {
@@ -142,13 +142,13 @@ resource "kubernetes_deployment" "quote_server_http" {
               port = var.port_http
 
               http_header {
-                name = "X-Healthz-Prober"
+                name  = "X-Healthz-Prober"
                 value = "readiness"
               }
             }
 
             initial_delay_seconds = 5
-            period_seconds = 5
+            period_seconds        = 5
           }
         }
       }
