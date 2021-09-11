b34893133812895850daba42c28dc6163dca6ed0
Dan Tulovsky <dant@lightstep.com>
2021-09-11 14:04:27 -0400

    fixed port

--- modules/quote-server/30-traffic-director-config.tf
+++ modules/quote-server/30-traffic-director-config.tf
@@ -9,7 +9,10 @@ resource "google_compute_health_check" "quote-server-grpc-health-check" {
   grpc_health_check {
     //    port_name          = "health-check-port"
     //    grpc_service_name  = "Quote"
-    port_specification = "USE_SERVING_PORT"
+    port_specification = "USE_FIXED_PORT"
+    port_name          = ""
+    # TODO: Automate
+    port = 8081
   }
 }
 
