a3bfdac4ede61612d67e191f632123a0f61b9b22
Dan Tulovsky <dant@lightstep.com>
2021-09-12 09:28:36 -0400

    s/http/grpc/g

--- modules/quote-server/main.tf
+++ modules/quote-server/main.tf
@@ -7,6 +7,8 @@ resource "kubectl_manifest" "servicemonitor_static_web_monitor" {
 
 # https://argoproj.github.io/argo-rollouts/migrating/
 resource "kubectl_manifest" "quote-server-rollout" {
-  depends_on = [kubernetes_deployment.quote_server_http]
-  yaml_body  = file("${path.module}/yaml/k8s/rollout.yaml")
+  depends_on = [
+    kubernetes_deployment.quote_server_grpc
+  ]
+  yaml_body = file("${path.module}/yaml/k8s/rollout.yaml")
 }
