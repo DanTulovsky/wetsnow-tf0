6cb2f834ae3a82c4739566fba5692e428d06871c
Dan Tulovsky <dant@lightstep.com>
2021-09-12 09:22:34 -0400

    disable http port

--- modules/quote-server/10-service.tf
+++ modules/quote-server/10-service.tf
@@ -1,35 +1,35 @@
-resource "kubernetes_service" "quote_server_http" {
-  metadata {
-    name      = "quote-server-http"
-    namespace = var.namespace
-
-    labels = {
-      app       = "quote"
-      component = "server-http"
-      service   = "quote-server-http"
-      tier      = "production"
-    }
-  }
-
-  spec {
-    port {
-      name        = "http"
-      protocol    = "TCP"
-      port        = var.port_http
-      target_port = "http"
-    }
-
-    selector = {
-      app       = "quote"
-      component = "server-http"
-      tier      = "production"
-    }
-
-    cluster_ip       = "None"
-    type             = "ClusterIP"
-    session_affinity = "None"
-  }
-}
+//resource "kubernetes_service" "quote_server_http" {
+//  metadata {
+//    name      = "quote-server-http"
+//    namespace = var.namespace
+//
+//    labels = {
+//      app       = "quote"
+//      component = "server-http"
+//      service   = "quote-server-http"
+//      tier      = "production"
+//    }
+//  }
+//
+//  spec {
+//    port {
+//      name        = "http"
+//      protocol    = "TCP"
+//      port        = var.port_http
+//      target_port = "http"
+//    }
+//
+//    selector = {
+//      app       = "quote"
+//      component = "server-http"
+//      tier      = "production"
+//    }
+//
+//    cluster_ip       = "None"
+//    type             = "ClusterIP"
+//    session_affinity = "None"
+//  }
+//}
 
 resource "kubernetes_service" "quote_server_grpc" {
   metadata {
