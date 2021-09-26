fd707ae39a266fbf2428a5b780c0ac67ab5281d0
Dan Tulovsky <dant@lightstep.com>
2021-09-26 16:04:11 -0400

    cleanup

--- modules/kubernetes-external-secrets/main.tf
+++ modules/kubernetes-external-secrets/main.tf
@@ -1,5 +1,4 @@
 resource "helm_release" "kubernetes-external-secrets" {
-  #   depends_on   = [kubernetes_secret.keycloak-db-secret]
   name         = "kubernetes-external-secrets"
   namespace    = var.namespace
   repository   = "https://external-secrets.github.io/kubernetes-external-secrets"
