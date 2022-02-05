528791f46f58835fa0afb8b757ff4470a66a9712
Dan Tulovsky <dant@lightstep.com>
2022-02-05 13:26:15 -0500

    cleanup

--- modules/open-telemetry/variables.tf
+++ modules/open-telemetry/variables.tf
@@ -23,7 +23,6 @@ variable "image_version" {
 variable "otel" {
   type = map(string)
   default = {
-    //    metrics_receivers  = "[otlp, k8s_cluster, kubeletstats]"
     metrics_receivers  = "[otlp, statsd, k8s_cluster, kubeletstats]"
     metrics_processors = "[memory_limiter, batch]"
     metrics_exporters  = "[otlp/lightstep]"
