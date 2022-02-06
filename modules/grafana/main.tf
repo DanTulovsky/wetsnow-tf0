44729dbc161f70ecf526ee2d8926576b0482560c
Dan Tulovsky <dant@lightstep.com>
2022-02-06 09:38:41 -0500

    fix

--- modules/grafana/main.tf
+++ modules/grafana/main.tf
@@ -17,12 +17,6 @@ resource "helm_release" "grafana" {
   })]
 }
 
-resource "kubectl_manifest" "grafana-monitor" {
-  count      = var.prom_enabled ? 1 : 0
-  depends_on = [helm_release.grafana]
-  yaml_body  = file("${path.module}/yaml/k8s/monitor.yaml")
-}
-
 //resource "kubernetes_secret" "grafana-env-secret" {
 //  metadata {
 //    name      = "grafana-env-secret"
--- modules/quote-server/main.tf
+++ modules/quote-server/main.tf
@@ -1,10 +1,3 @@
-resource "kubectl_manifest" "servicemonitor_static_web_monitor" {
-  count = var.prom_enabled ? 1 : 0
-  yaml_body = templatefile("${path.module}/yaml/k8s/00-monitoring-prom.yaml", {
-    namespace = var.namespace
-  })
-}
-
 # https://argoproj.github.io/argo-rollouts/migrating/
 resource "kubectl_manifest" "quote-server-rollout" {
   depends_on = [
