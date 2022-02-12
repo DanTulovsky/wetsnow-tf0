74680a4bdb3bd785eb94424fbe03a6be36813c9b
Dan Tulovsky <dant@lightstep.com>
2022-02-12 10:16:32 -0500

    argo rollouts

--- modules/argo/main.tf
+++ modules/argo/main.tf
@@ -5,7 +5,7 @@ resource "helm_release" "argo-rollouts" {
   chart        = "argo-rollouts"
   wait         = true
   force_update = true
-  version      = "2.2.0"
+  version      = "2.9.3"
 
   values = [templatefile("${path.module}/yaml/values.yaml", {
     argo_version = var.argo_version
