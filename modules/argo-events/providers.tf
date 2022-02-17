8ea820f803ddb134075611d2a7ed131c4571db73
Dan Tulovsky <dant@lightstep.com>
2022-02-17 08:39:12 -0500

    add provider

--- modules/argo-events/providers.tf
+++ modules/argo-events/providers.tf
@@ -8,5 +8,9 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
   }
 }
