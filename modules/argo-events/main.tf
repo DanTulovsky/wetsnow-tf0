b40a742ffa2e5bb3d0ab6af68d47078fa5c73b72
Dan Tulovsky <dant@lightstep.com>
2022-02-17 08:36:54 -0500

    fix names

--- modules/argo-events/main.tf
+++ modules/argo-events/main.tf
@@ -1,17 +1,17 @@
 data "kubectl_file_documents" "argo-events-docs-install" {
-  content = file("install-validating-webhook.yaml")
+  content = file("yaml/install-validating-webhook.yaml")
 }
 
-resource "kubectl_manifest" "argo-events" {
+resource "kubectl_manifest" "argo-events-install" {
   for_each  = data.kubectl_file_documents.argo-events-docs-install.manifests
   yaml_body = each.value
 }
 
 data "kubectl_file_documents" "argo-events-docs-eventbus" {
-  content = file("eventbus.yaml")
+  content = file("yaml/eventbus.yaml")
 }
 
-resource "kubectl_manifest" "argo-events" {
+resource "kubectl_manifest" "argo-events-eventbus" {
   for_each  = data.kubectl_file_documents.argo-events-docs-eventbus.manifests
   yaml_body = each.value
 }
