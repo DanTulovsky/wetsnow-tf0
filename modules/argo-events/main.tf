7ee167cdfd8e5e0da3afeb2ecab8b1aa0e81eca5
Dan Tulovsky <dant@lightstep.com>
2022-02-17 08:52:34 -0500

    add install

--- modules/argo-events/main.tf
+++ modules/argo-events/main.tf
@@ -1,9 +1,18 @@
 data "kubectl_file_documents" "argo-events-docs-install" {
-  content = file("${path.module}/yaml/install-validating-webhook.yaml")
+  content = file("${path.module}/yaml/install.yaml")
 }
 
 resource "kubectl_manifest" "argo-events-install" {
   for_each  = data.kubectl_file_documents.argo-events-docs-install.manifests
+  yaml_body = each.value
+}
+
+data "kubectl_file_documents" "argo-events-docs-install-webhook" {
+  content = file("${path.module}/yaml/install.yaml")
+}
+
+resource "kubectl_manifest" "argo-events-install" {
+  for_each  = data.kubectl_file_documents.argo-events-docs-install-webhook.manifests
   yaml_body = each.value
 }
 
