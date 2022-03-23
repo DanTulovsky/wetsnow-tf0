46315bc98f8565186ebeee076e1e47f61bb40b47
Dan Tulovsky <dant@lightstep.com>
2022-03-22 22:28:44 -0400

    agent

---
+++ modules/nobl9/agent.tf
@@ -0,0 +1,9 @@
+resource "nobl9_agent" "lightstep0" {
+  agent_type = "k8s"
+  name       = "lightstep0"
+  project    = nobl9_project.wetsnow.name
+  lightstep_config = {
+    organization = "wetsnow-550a21f7"
+    project      = "wetsnow-dev-550a21f7"
+  }
+}
