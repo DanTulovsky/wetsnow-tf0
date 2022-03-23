76c84dea5d41de4dcca955216d1a41972c666495
Dan Tulovsky <dant@lightstep.com>
2022-03-22 22:07:36 -0400

    nobl9 project

--- modules/nobl9/project.tf
+++ modules/nobl9/project.tf
@@ -1,5 +1,5 @@
 resource "nobl9_project" "wetsnow" {
-  display_name = "wetsnow"
-  name         = "wetsnow"
+  display_name = "wetsnow0"
+  name         = "wetsnow0"
   description  = "Wetsnow Nobl9 Project"
 }
