757dce32ef1c6b472fa65f3bfebaa6c6b12008a3
Dan Tulovsky <dant@lightstep.com>
2022-03-22 22:18:06 -0400

    latency

---
+++ modules/nobl9/slos.tf
@@ -0,0 +1,35 @@
+# https://docs.nobl9.com/Sources/lightstep/
+resource "nobl9_slo" "this" {
+  name             = "${nobl9_service.web-static.name}-latency"
+  service          = nobl9_service.web-static.name
+  budgeting_method = "Occurrences"
+  project          = nobl9_project.wetsnow.name
+
+  alert_policies = [
+    "Default Alert"
+  ]
+
+  time_window {
+    unit       = "Day"
+    count      = 28
+    is_rolling = true
+  }
+
+  objective {
+    target       = 0.99
+    display_name = "OK"
+    value        = 60
+    op           = "lte"
+  }
+
+  indicator {
+    name = "webstatic_latency"
+    raw_metric {
+      lightstep {
+        steam_id     = "XTLbMfjL"
+        type_of_data = "latency"
+        percentile   = "99"
+      }
+    }
+  }
+}
