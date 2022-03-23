15e7969e7a5f97e545c356a0057ae12d179a9181
Dan Tulovsky <dant@lightstep.com>
2022-03-23 09:27:43 -0400

    avaulability

--- modules/nobl9/slos.tf
+++ modules/nobl9/slos.tf
@@ -38,3 +38,42 @@ resource "nobl9_slo" "web_static_latency" {
     }
   }
 }
+
+resource "nobl9_slo" "web_static_availability" {
+  name             = "${nobl9_service.web_static.name}-availability"
+  service          = nobl9_service.web_static.name
+  budgeting_method = "Occurrences"
+  project          = nobl9_project.wetsnow.name
+
+  alert_policies = [
+    nobl9_alert_policy.default_alert_policy.name,
+  ]
+
+  time_window {
+    unit       = "Day"
+    count      = 28
+    is_rolling = true
+  }
+
+  objective {
+    target       = 0.99
+    display_name = "OK"
+    value        = 60
+    op           = "lte"
+  }
+
+  indicator {
+    #    name    = nobl9_agent.lightstep0.name
+    # This is the name of the agent
+    name = "lightstep"
+    #    kind    = "agent"
+    project = nobl9_project.wetsnow.name
+
+    raw_metric {
+      lightstep {
+        stream_id    = "XTLbMfjL"
+        type_of_data = "good"
+      }
+    }
+  }
+}
