31a4079379f4cf6516dd9bf5d55c72a5846d03fe
Dan Tulovsky <dant@lightstep.com>
2022-03-24 22:08:15 -0400

    refactor

--- modules/nobl9/alert_policies.tf
+++ modules/nobl9/alert_policies.tf
@@ -1,35 +1,3 @@
-resource "nobl9_alert_method_email" "default" {
-  name    = "default-email-method"
-  project = nobl9_project.wetsnow.name
-  subject = "Your SLO $slo_name needs attention!"
-  body    = <<EOT
-$alert_policy_name has triggered with the following conditions:
-$alert_policy_conditions[]
-
-Time: $timestamp
-Severity: $severity
-Project: $project_name
-Service: $service_name
-Organization: $organization
-Labels:
-- SLO: $slo_labels_text
-- Service: $service_labels_text
-- Alert Policy: $alert_policy_labels_text
-EOT
-  to = [
-    "dant@wetsnow.com"
-  ]
-}
-
-resource "nobl9_alert_method_jira" "jira_ticket" {
-  name        = "jira-ticket"
-  project     = nobl9_project.wetsnow.name
-  project_key = "SELF"
-  url         = "https://wetsnow0.atlassian.net/"
-  username    = "dant@wetsnow.com"
-  apitoken    = var.jira_api_token
-}
-
 resource "nobl9_alert_policy" "web_static_error_budget_alert_policy" {
   name         = "web-static"
   project      = nobl9_project.wetsnow.name
