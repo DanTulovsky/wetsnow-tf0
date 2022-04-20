786cfcf20549f47ab499c84d627ebbd9c282d89b
Dan Tulovsky <dant@lightstep.com>
2022-04-19 22:09:28 -0400

    clear

--- modules/http-ingress/managed-certs.tf
+++ modules/http-ingress/managed-certs.tf
@@ -6,30 +6,6 @@
 # then remove the previous instance
 # this causes downtime!!
 
-# new
-resource "google_compute_managed_ssl_certificate" "wetsnow-com-20220419" {
-  name = "wetsnow-com-20220419"
-  managed {
-    domains = sort([
-      "ambassador-admin.wetsnow.com.",
-      "argocd.wetsnow.com.",
-      "dusselskolk.com.",
-      "grafana.wetsnow.com.",
-      "kubecost.wetsnow.com.",
-      "login.wetsnow.com.",
-      "pepper-poker.wetsnow.com",
-      "pgadmin.wetsnow.com.",
-      "prometheus.wetsnow.com.",
-      "rollouts.wetsnow.com",
-      "scope.wetsnow.com",
-      "weave-scope.wetsnow.com",
-      "wetsnow.com.",
-      "www.dusselskolk.com.",
-      "www.wetsnow.com.",
-    ])
-  }
-}
-
 resource "google_compute_managed_ssl_certificate" "wetsnow-com-20220419-00" {
   name = "wetsnow-com-20220419-00"
   managed {
