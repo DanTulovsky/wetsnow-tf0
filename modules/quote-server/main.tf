8b70e76629a180c65edd207171df32155c9e9b65
Dan Tulovsky <dant@wetsnow.com>
2023-08-20 20:54:18 -0400

    1 replica

--- modules/quote-server/main.tf
+++
@@ -1,7 +0,0 @@
-# https://argoproj.github.io/argo-rollouts/migrating/
-resource "kubectl_manifest" "quote-server-rollout" {
-  depends_on = [
-    kubernetes_deployment_v1.quote_server_grpc
-  ]
-  yaml_body = file("${path.module}/yaml/k8s/rollout.yaml")
-}
